f042110627fab110a46141a3e2e43ea2f938f3f4
Ariff Rozi <e175756@ie.u-ryukyu.ac.jp>
2019-12-03 15:55:19 +0900

    Initial Commit

---
+++ PhoneAppProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhoneAppProject
+//
+//  Created by Ariff Rozi on 2019/12/03.
+//  Copyright © 2019 ryukyu team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
